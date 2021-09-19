fc2865586168b794ea5aa98a2e12334b22a33a97
VincetCampos <62453298+VincetCampos@users.noreply.github.com>
2021-09-18 23:02:28 -0600

    Cree el abstract factory

---
+++ patterns_1_abstract_factory_ejercicio/src/org/formacion/abstractfactory/AbstractFactory.java
@@ -0,0 +1,8 @@
+package org.formacion.abstractfactory;
+
+public interface AbstractFactory {
+	
+	public Preguntas createPreguntas();
+	
+	public Saludos createSaludos();
+}
--- patterns_1_abstract_factory_ejercicio/test/org/formacion/abstractfactory/TestMultiIdioma.java
+++ patterns_1_abstract_factory_ejercicio/test/org/formacion/abstractfactory/TestMultiIdioma.java
@@ -5,16 +5,21 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestMultiIdioma {
-
+	
+	AbstractFactory factory;
+	
 	@Test
 	public void test_es() {
 		//implementar Abstract factory
-		Preguntas preguntas = null;
+		factory = new FactoryEs();
+		
+		Preguntas preguntas = factory.createPreguntas();
 		
 		assertEquals("¿qué hora es?", preguntas.preguntaHora() );
 		assertEquals("¿qué tiempo hace?", preguntas.preguntaTiempo() );
 		//implementar Abstract factor
-		Saludos saludos = null;
+		
+		Saludos saludos = factory.createSaludos();
 		
 		assertEquals("buenos días", saludos.buenosDias());
 		assertEquals("buenas tardes", saludos.buenasTardes());
@@ -23,13 +28,16 @@ public class TestMultiIdioma {
 	@Test
 	public void test_en() {
 		//implementar Abstract factor
-		Preguntas preguntas = null;
+		
+		factory = new FactoryEn();
+		
+		Preguntas preguntas = factory.createPreguntas();
 		
 		assertEquals("what time is it?", preguntas.preguntaHora() );
 		assertEquals("how is the weather?", preguntas.preguntaTiempo() );
 		
 		//implementar Abstract factor
-		Saludos saludos = null;
+		Saludos saludos = factory.createSaludos();
 		
 		assertEquals("good morning", saludos.buenosDias());
 		assertEquals("good afternoon", saludos.buenasTardes());
